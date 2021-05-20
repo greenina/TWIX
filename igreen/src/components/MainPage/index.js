5b52957a2ebcb23bbdca08d2efda93bc01f44ed9
hotsunchip <hotsucnhip@kaist.ac.kr>
2021-05-20 09:10:26 +0900

    Modification of MainPage

--- igreen/src/components/MainPage/index.js
+++ igreen/src/components/MainPage/index.js
@@ -1,22 +1,65 @@
 import './style.css';
+import {db, firebaseApp, firebase} from '../../firebase';
+import React, { useEffect, useState } from 'react';
+import Switch from './Switch';
 
 function MainPage() {
-  const mv2category = () => {
-    document.location.href = '/category';
+  /**/
+  const resizeArea = (data) => {
+    const winWidth = document.body.offsetWidth;
+    let a = [];
+    for (let i=0; i<data.length; i++) {
+      a.push(data[i]*winWidth/1440);
+    }
+    return a.join();
   };
-  const mv2detail = () => {
-    document.location.href = '/detail';
-  };
-  const mv2mypage = () => {
-    document.location.href = '/mypage';
-  };
+  const [windowSize, setWindowSize] = useState(document.body.offsetWidth);
+  const handleResize = () => {
+      setWindowSize({
+        winWidth: window.innerWidth
+      });
+    }
+    
+    useEffect(() => {
+      window.addEventListener('resize', handleResize);
+      return () => { // cleanup 
+        window.removeEventListener('resize', handleResize);
+      }
+    }, [windowSize]);
 
   return (
     <div>
-      <div> 메인 페이지</div>
-      <button onClick={mv2category}> 제품 카테고리 페이지 </button>
-      <button onClick={mv2detail}> 제품 상세 페이지</button>
-      <button onClick={mv2mypage}> 마이페이지 </button>
+      <div class="slider">
+        <div class="slides">
+          <input type="radio" name="radio-btn" id="radio-lr" defaultChecked></input>
+          <input type="radio" name="radio-btn" id="radio-kc"></input>
+          <input type="radio" name="radio-btn" id="radio-br"></input>
+          <input type="radio" name="radio-btn" id="radio-mr"></input>
+        
+          <div class="slide first">
+            <img src="https://ifh.cc/g/il5UZy.png" alt="" usemap="#livingroom-map"></img>
+            <map id="livingroom-map" name="livingroom-map" width="100%" height="100%" border="5px solid #000000">
+              <area shape="poly" alt="" title="" coords={resizeArea([204,260,242,240,277,255,261,210,286,197,297,226,316,235,302,261,375,281,382,341,317,355,197,315,202,256,203,262])} href="/category" target="" />
+              <area shape="poly" alt="" title="" coords={resizeArea([540,603,467,720,483,768,835,768,1078,689,1118,636,1081,538,970,493,820,493,537,600])} href="/category" target="" /></map>
+          </div>
+          <div class="slide">
+            <img src="https://ifh.cc/g/il5UZy.png" alt=""></img>
+          </div>
+          <div class="slide">
+            <img src="https://ifh.cc/g/il5UZy.png" alt=""></img>
+          </div>
+          <div class="slide">
+            <img src="https://ifh.cc/g/il5UZy.png" alt=""></img>
+          </div>
+          <div class="navigation-manual">
+            <label for="radio-lr" class="manual-btn-lr"></label>
+            <label for="radio-kc" class="manual-btn-kc"></label>
+            <label for="radio-br" class="manual-btn-br"></label>
+            <label for="radio-mr" class="manual-btn-mr"></label>
+          </div>
+        </div>
+      </div>
+      <Switch />
     </div>
   );
 }
